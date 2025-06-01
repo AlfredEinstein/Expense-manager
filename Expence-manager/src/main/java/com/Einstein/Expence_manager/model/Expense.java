c5075b86d74338cecf054f0cb24bd11375a940d2
Einstein <alfred26092001@gmail.com>
2025-06-01 21:52:25 +0530

    Server Initial commit

---
+++ Expence-manager/src/main/java/com/Einstein/Expence_manager/model/Expense.java
@@ -0,0 +1,30 @@
+package com.Einstein.Expence_manager.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "expenses")
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    private String comment;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
---
+++ Expence-manager/src/test/java/com/Einstein/Expence_manager/ExpenceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Einstein.Expence_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
